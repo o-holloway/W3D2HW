936b9a89d02c71c36814d052c30e8013aeac6084
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-03-06 11:34:52 -0500

    initial commit

---
+++ geometry.py
@@ -0,0 +1,7 @@
+import math
+
+def areaofcircle(radius):
+    return round(math.pi * (radius ** 2),2)
+
+def hypotenuse(sidea,sideb):
+    return f"{round(math.sqrt((sidea ** 2) + (sideb ** 2)),0)} inches"
